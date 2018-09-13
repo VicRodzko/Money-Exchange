c086cc2b0a20283d630efff0791e724e7d47767e
Victor Rodzko <vicrodzko@mail.ru>
2018-09-13 12:51:27 +0300

    How much BIG money do you need

--- src/index.js
+++ src/index.js
@@ -1,5 +1,52 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	if (currency < 10000 && currency > 0) {
+		let obj = {};
+		
+		let h = 50,
+			q = 25,
+			d = 10,
+			n = 5,
+			p = 1;
+
+		if (currency/h >= 1) {
+			obj["H"] = Math.floor(currency/h);
+			if ((currency/h) >= obj["H"]) {
+				currency = currency - obj["H"] * h;
+			}
+		}
+
+		if (currency/q >= 1 && currency != 0) {
+			obj["Q"] = Math.floor(currency/q);
+			if ((currency/q) >= obj["Q"]) {
+				currency = currency - obj["Q"] * q;
+			}
+		}
+
+		if (currency/d >= 1 && currency != 0) {
+			obj["D"] = Math.floor(currency/d);
+			if ((currency/d) >= obj["D"]) {
+				currency = currency - obj["D"] * d;
+			}
+		}
+
+		if (currency/n >= 1 && currency != 0) {
+			obj["N"] = Math.floor(currency/n);
+			if ((currency/n) >= obj["N"]) {
+				currency = currency - obj["N"] * n;
+			}
+		}
+
+		if (currency/p >= 1 && currency != 0) {
+			obj["P"] = Math.floor(currency/p);
+			if ((currency/p) >= obj["P"]) {
+				currency = currency - obj["P"] * p;
+			}
+        }
+
+        return obj;
+        
+    } else if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else return {};
 }
